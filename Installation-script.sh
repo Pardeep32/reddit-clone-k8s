b3103c17098cd0b12967a0a82d40f764c54bed1b
pardeep kaur <137564227+Pardeep32@users.noreply.github.com>
2024-03-25 16:54:10 -0400

    Update Installation-script.sh

--- Installation-script.sh
+++ Installation-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt update -y
+
+###install java
 sudo touch /etc/apt/keyrings/adoptium.asc
 sudo wget -O /etc/apt/keyrings/adoptium.asc https://packages.adoptium.net/artifactory/api/gpg/key/public
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
